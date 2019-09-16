ba4a0b5a3971e679ac03152e5b6e5bb4927a4143
weiwei <371964500@qq.com>
2019-09-16 18:02:30 +0800

    kafka

---
+++ kafka/src/main/java/com/weisir/kafka/raw/api/KafkaProducerDemo.java
@@ -0,0 +1,44 @@
+package com.weisir.kafka.raw.api;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * <p></p>
+ *
+ * @author: 研发部-魏巍
+ * @since: 2019/9/15 18:09
+ * @version: v2.6.3
+ */
+public class KafkaProducerDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 初始化配置
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","47.107.123.189:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        // 创建 kafaka Producer
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer(properties);
+
+        // 创建 Kakfa 消息  = ProducerRecord
+        String topic = "gupao111";
+        Integer partition = 0;
+        Long timestamp = System.currentTimeMillis();
+        String key = "message-key";
+        String value = "caonima";
+        ProducerRecord<String,String> producerRecord = new ProducerRecord(topic,partition,timestamp,key,value);
+
+        // 发送消息
+        Future<RecordMetadata> future = kafkaProducer.send(producerRecord);
+        //强制执行
+        future.get();
+
+    }
+}
---
+++ kafka/src/main/java/com/weisir/kafka/web/controller/SpringKafKapProducerController.java
@@ -0,0 +1,36 @@
+package com.weisir.kafka.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p></p>
+ *
+ * @author: 研发部-魏巍
+ * @since: 2019/9/15 23:00
+ * @version: v2.6.3
+ */
+@RestController
+public class SpringKafKapProducerController {
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    private final String topic;
+
+    @Autowired
+    public SpringKafKapProducerController(KafkaTemplate<String,String> template, @Value("${kafka.topic}") String topic){
+        this.kafkaTemplate = template;
+        this.topic = topic;
+    }
+
+    @PostMapping("/messag/send")
+    public Boolean send(@RequestParam String message){
+        kafkaTemplate.send(topic,message);
+        return true;
+    }
+
+}
